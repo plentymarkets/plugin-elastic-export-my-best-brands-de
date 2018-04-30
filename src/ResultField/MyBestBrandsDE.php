2f3415d8a46e036b2e453dc27c934e7fc5c59eb0
Max-Nils Brüschke <max.brueschke@plentymarkets.com>
2018-04-30 13:08:27 +0200

    laravel 5.5 update

--- src/ResultField/MyBestBrandsDE.php
+++ src/ResultField/MyBestBrandsDE.php
@@ -43,7 +43,9 @@ class MyBestBrandsDE extends ResultFields
         $settings = $this->arrayHelper->buildMapFromObjectList($formatSettings, 'key', 'value');
         $reference = $settings->get('referrerId') ? $settings->get('referrerId') : -1;
 
-		$this->setOrderByList(['item.id', ElasticSearch::SORTING_ORDER_ASC]);
+		$this->setOrderByList([
+			'path' => 'item.id',
+			'order' => ElasticSearch::SORTING_ORDER_ASC]);
 
         $itemDescriptionFields = ['texts.urlPath'];
         $itemDescriptionFields[] = 'texts.keywords';
